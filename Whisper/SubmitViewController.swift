345708e7f67ae0eae343bd7063a20f60f2152ad7
Kabir singh <agouraappclub@gmail.com>
2022-10-19 20:51:45 -0700

    test

---
+++ Whisper/SubmitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubmitViewController.swift
+//  Whisper
+//
+//  Created by AgouraAppClub on 10/19/22.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
